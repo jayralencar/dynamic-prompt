0060ca6b2637da2d5d09fd3c9e9df3a202bcf38d
Ubuntu <azureuser@jayr.0gmr22muk1cehgdzirbic2e3xg.cx.internal.cloudapp.net>
2022-04-25 13:05:55 +0000

    copa ok

--- download_data.sh
+++ download_data.sh
@@ -4,3 +4,7 @@ cd data
 wget https://dl.fbaipublicfiles.com/glue/superglue/data/v2/COPA.zip
 unzip COPA.zip
 rm COPA.zip
+
+wget https://dl.fbaipublicfiles.com/glue/superglue/data/v2/BoolQ.zip
+unzip BoolQ.zip
+rm BoolQ.zip
